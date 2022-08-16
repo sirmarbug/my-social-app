750c475683824b161cf8880b0df4d4b502fea760
Mariusz Bugajski <mariusz@bugajski.dev>
2022-08-16 08:28:47 +0200

    frontend: add mui

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import Button from '@mui/material/Button';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  const increment = () => {
+    setCounter(counter + 1)
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {counter}
+      <Button onClick={increment}>+</Button>
+      <Button onClick={decrement}>-</Button>
     </div>
   );
 }
