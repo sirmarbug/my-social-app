86177b4c429d4ca1293bb6f44cb2dabc3a08c492
Mariusz Bugajski <mariusz@bugajski.dev>
2022-08-18 13:21:42 +0200

    frontend: implement not found page

--- web/src/pages/NotFound.js
+++ web/src/pages/NotFound.js
@@ -1,8 +1,31 @@
+import AuthLayout from "../layouts/AuthLayout";
+import {Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import {useHistory} from "react-router-dom";
+
 const NotFound = () => {
+    const history = useHistory()
+
+    const goToHomePage = () => {
+        history.push('/')
+    }
+
     return (
-        <div>
-            <h1>NotFound</h1>
-        </div>
+        <AuthLayout>
+            <Typography mb={2} variant="h4">
+                Nie znaleziono strony
+            </Typography>
+            <Typography mb={4} variant="body1">
+                Przejdź na stronę główna
+            </Typography>
+            <Button
+                variant="contained"
+                size="small"
+                onClick={goToHomePage}
+            >
+                Strona główna
+            </Button>
+        </AuthLayout>
     )
 }
 
